30acdcf52ffcf3d03196e9e4895e26981a9995be
Qianqian Hu <qianhu@pindous.com>
2018-01-26 21:23:08 -0800

    adds constrctor and writes prototype to fullfill story

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,21 @@
 //business logic
-function Portfolio(fragranceIndex, aromaIndex, sweetnessIndex, acidityIndex, bodyIndex, afterTasteIndex){
-  this.fragrance = fragranceIndex;
-  this.aroma = aromaIndex;
-  this.sweetness = sweetnessIndex;
-  this.acidity = acidityIndex;
-  this.body = bodyIndex;
-  this.afterTaste = afterTasteIndex;
-  
+function Portfolio(totalFragrance, totalAroma, totalSweetness, totalAcidity, totalBody, totalAfterTaste){
+  this.fragrance = totalFragrance;
+  this.aroma = totalAroma;
+  this.sweetness = totalSweetness;
+  this.acidity = totalAcidity;
+  this.body = totalBody;
+  this.afterTaste = totalAfterTaste;
+}
+
+Portfolio.prototype.getAdj = function(){
+  if ((this.sweetness <= 10) &&(this.acidity <= 10)) {
+    return "restrain";
+  } else if ((this.sweetness > 20) &&(this.acidity > 20)) {
+    return "gushy";
+  }else {
+    return "curious";
+  }
 }
 
 //user interface logic
@@ -45,32 +54,63 @@ $(document).ready(function() {
   $("form#formOne").submit(function(event){
      event.preventDefault();
       var fragranceList = [];
-  //     var aromaList = [];
-  //     var sweetnessList = [];
-  //     var acidityList = [];
-  //     var bodyList = [];
-  //     var afterTasteList = [];
-  //
+      var aromaList = [];
+      var sweetnessList = [];
+      var acidityList = [];
+      var bodyList = [];
+      var afterTasteList = [];
+
      $("input:checkbox[name=totalForm]:checked").each(function(){
-      var tasteResult = $(this).val();
-      var tasteResultToArray = tasteResult.split(",");
-      var fragrance = tasteResultToArray[0];
+      var tasteResult = $(this).val().split(",");
+
+      var fragrance = tasteResult[0];
+      var aroma = tasteResult[1];
+      var sweetness = tasteResult[2];
+      var acidity = tasteResult[3];
+      var body = tasteResult[4];
+      var afterTaste = tasteResult[5];
 
       fragranceList.push(parseInt(fragrance));
-  //     var aroma = tasteResult[1];
-  //     var sweetness = tasteResult[2];
-  //     var acidity = tasteResult[3];
-  //     var body = tasteResult[4];
-  //     var afterTaste = tasteResult[5];
-      var totalFragrance = 0;
-      fragranceList.forEach(function(score) {
-        totalFragrance += score;
-      });
-
-      if (totalFragrance > 6 ) {
-        alert(totalFragrance);
-      }
+      aromaList.push(parseInt(aroma));
+      sweetnessList.push(parseInt(sweetness));
+      acidityList.push(parseInt(acidity));
+      bodyList.push(parseInt(body));
+      afterTasteList.push(parseInt(afterTaste));
 
     });
+
+    var totalFragrance = 0;
+    fragranceList.forEach(function(score) {
+      totalFragrance += score;
+    });
+    var totalAroma = 0;
+    aromaList.forEach(function(score) {
+      totalAroma += score;
+    });
+    var totalSweetness = 0;
+    sweetnessList.forEach(function(score) {
+      totalSweetness += score;
+    });
+    var totalAcidity = 0;
+    acidityList.forEach(function(score) {
+      totalAcidity += score;
+    });
+    var totalBody = 0;
+    bodyList.forEach(function(score) {
+      totalBody += score;
+    });
+    var totalAfterTaste = 0;
+    afterTasteList.forEach(function(score) {
+      totalAfterTaste += score;
+    });
+
+
+      $(".story").show();
+      $("#adj").text(Portfolio.getAdj);
+      $("#noun").text("menu");
+      $("#tasteAdj").text("bitter");
+      $("#phrase").text("wash dishes");
+
+
   });
 });
